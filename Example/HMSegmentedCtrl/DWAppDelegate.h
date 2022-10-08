cb13a1cf4327b717ea0524471c6e64e96b57b551
mark <mark@douwandeMacBook-Air.local>
2022-10-08 14:29:02 +0800

    Initial commit

---
+++ Example/HMSegmentedCtrl/DWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DWAppDelegate.h
+//  HMSegmentedCtrl
+//
+//  Created by mark on 10/08/2022.
+//  Copyright (c) 2022 mark. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/HMSegmentedCtrl/DWViewController.h
@@ -0,0 +1,13 @@
+//
+//  DWViewController.h
+//  HMSegmentedCtrl
+//
+//  Created by mark on 10/08/2022.
+//  Copyright (c) 2022 mark. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DWViewController : UIViewController
+
+@end
